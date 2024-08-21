05434cf2a5f376b901494efc4284fee069c0f22c
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-08-21 16:55:45 +0300

    Implement publisher module

---
+++ publisher/go.mod
@@ -0,0 +1,20 @@
+module github.com/qsoulior/wb-l0/publisher
+
+go 1.22.4
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/nats-io/stan.go v0.10.4
+)
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/nats-io/nats-server/v2 v2.10.18 // indirect
+	github.com/nats-io/nats-streaming-server v0.25.6 // indirect
+	github.com/nats-io/nats.go v1.37.0 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
---
+++ publisher/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/nats-io/stan.go"
+)
+
+type Config struct {
+	URL       string `json:"url"`
+	ClusterID string `json:"cluster_id"`
+	ClientID  string `json:"client_id"`
+	Subject   string `json:"subject"`
+	Interval  int    `json:"interval"`
+	Count     int    `json:"count"`
+}
+
+var (
+	DefaultInterval = 10 * time.Second
+	DefaultFile     = "model.json"
+)
+
+func NewConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := new(Config)
+	d := json.NewDecoder(f)
+	err = d.Decode(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func main() {
+	var path string
+	flag.StringVar(&path, "c", "", "config file path")
+	flag.Parse()
+
+	if path == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	cfg, err := NewConfig(path)
+	if err != nil {
+		logger.Error("failed to load config", "err", err)
+		return
+	}
+
+	f, err := os.Open(DefaultFile)
+	if err != nil {
+		logger.Error("failed to load model file", "err", err)
+		return
+	}
+
+	order := new(Order)
+	d := json.NewDecoder(f)
+	err = d.Decode(order)
+	if err != nil {
+		logger.Error("failed to decode model", "err", err)
+		return
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	sc, err := stan.Connect(cfg.ClusterID, cfg.ClientID, stan.NatsURL(cfg.URL))
+	if err != nil {
+		logger.Error("failed to establish stan connection", "err", err)
+		return
+	}
+	logger.Info("stan connection established", "url", cfg.URL)
+
+	interval := DefaultInterval
+	if cfg.Interval > 0 {
+		interval = time.Duration(cfg.Interval) * time.Second
+	}
+
+	ticker := time.NewTicker(interval)
+	for count := 0; count < cfg.Count || cfg.Count <= 0; count++ {
+		select {
+		case <-ticker.C:
+			order.OrderUID = uuid.New().String()
+			data, err := json.Marshal(order)
+			if err != nil {
+				logger.Error("failed to encode message", "err", err)
+				continue
+			}
+
+			err = sc.Publish(cfg.Subject, data)
+			if err != nil {
+				logger.Error("failed to publish message", "err", err)
+				continue
+			}
+			logger.Info("message sended", "uid", order.OrderUID)
+		case <-ctx.Done():
+			logger.Info("notify signal accepted")
+			ticker.Stop()
+			return
+		}
+	}
+}
